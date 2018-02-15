a72e1ac8dc47d47944498c74592e435cc16c1a36
Daniel Welch <dwelch2102@gmail.com>
2018-02-15 17:24:22 -0500

    travis change: push everything to docker with either latest tag or build tag, as determined by hassio-addons/build-env --git tag

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -ev
 if [ -z ${TRAVIS_TAG} ]; then
-    echo "Untagged build. Exiting..."
-    exit 0
+    echo "Untagged build found."
 else
-    echo "New git tagged build found. Attempting to build..."
-    docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-    docker run -it --rm --privileged --name ${ADDON_NAME} \
-            -v ~/.docker:/root/.docker \
-            -v "$(pwd)":/docker \
-            hassioaddons/build-env:latest \
-            --target ${ADDON_NAME} \
-            --git \
-            --all \
-            --from "homeassistant/{arch}-base"
-            --author "Daniel Welch <dwelch2102@gmail.com>"
-            --doc-url "${GITHUB_URL}"
-    echo "Build successful"
+    echo "New git tagged build found."
 fi
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+docker run -it --rm --privileged --name ${ADDON_NAME} \
+        -v ~/.docker:/root/.docker \
+        -v "$(pwd)":/docker \
+        hassioaddons/build-env:latest \
+        --target ${ADDON_NAME} \
+        --git \
+        --all \
+        --from "homeassistant/{arch}-base"
+        --author "Daniel Welch <dwelch2102@gmail.com>"
+        --doc-url "${GITHUB_URL}"
+echo "Local Docker build successful."
---
+++ distribute.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+if [ ! -z ${TRAVIS_TAG} ]; then
+    echo "Tagged build found. Pushing to Docker with tag 'latest'."
+else
+    echo "No tag found. Pushing to Docker with tag 'test'."
+fi
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker run -it --rm --privileged --name "${ADDON_NAME}" \
+-v ~/.docker:/root/.docker \
+-v "$(pwd)":/docker \
+hassioaddons/build-env:latest \
+--target "${ADDON_NAME}" \
+--git \
+--all \
+--push \
+--from "homeassistant/{arch}-base" \
+--author "Daniel Welch <dwelch2102@gmail.com>" \
+--doc-url "${GITHUB_URL}"
+
