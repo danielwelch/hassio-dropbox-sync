370cd335a7c4ab89f369b52dcc043d8466d96d01
DiggiD <drowkavi@gmail.com>
2020-04-22 07:46:58 +0530

    backup all hassio folders or particular folder
    also listening for $cmd =  "all"

--- dropbox-sync/run.sh
+++ dropbox-sync/run.sh
@@ -6,12 +6,14 @@ TOKEN=$(jq --raw-output ".oauth_access_token" $CONFIG_PATH)
 OUTPUT_DIR=$(jq --raw-output ".output // empty" $CONFIG_PATH)
 KEEP_LAST=$(jq --raw-output ".keep_last // empty" $CONFIG_PATH)
 FILETYPES=$(jq --raw-output ".filetypes // empty" $CONFIG_PATH)
+FOLDER_BACKUP=$(jq --raw-output ".folder_backup // empty" $CONFIG_PATH)
 
 if [[ -z "$OUTPUT_DIR" ]]; then
     OUTPUT_DIR="/"
 fi
 
 echo "[Info] Files will be uploaded to: ${OUTPUT_DIR}"
+echo "[Info] Full backups will be uploaded to: ${OUTPUT_DIR}/hassio_backup"
 
 echo "[Info] Saving OAUTH_ACCESS_TOKEN to /etc/uploader.conf"
 echo "OAUTH_ACCESS_TOKEN=${TOKEN}" >> /etc/uploader.conf
@@ -27,6 +29,10 @@ while read -r msg; do
     if [[ $cmd = "upload" ]]; then
         echo "[Info] Uploading all .tar files in /backup (skipping those already in Dropbox)"
         ./dropbox_uploader.sh -s -f /etc/uploader.conf upload /backup/*.tar "$OUTPUT_DIR"
+        if [[ $cmd = "all" ]]; then
+            echo "[Info] Uploading all directories in Hassio root"
+            ./dropbox_uploader.sh -s -f /etc/uploader.conf upload / "${OUTPUT_DIR}/hassio_backup/"
+        fi
         if [[ "$KEEP_LAST" ]]; then
             echo "[Info] keep_last option is set, cleaning up files..."
             python3 /keep_last.py "$KEEP_LAST"
@@ -34,6 +40,10 @@ while read -r msg; do
         if [[ "$FILETYPES" ]]; then
             echo "[Info] filetypes option is set, scanning share directory for files with extensions ${FILETYPES}"
             find /share -regextype posix-extended -regex "^.*\.(${FILETYPES})" -exec ./dropbox_uploader.sh -s -f /etc/uploader.conf upload {} "$OUTPUT_DIR" \;
+        fi
+        if [[ "$FOLDER_BACKUP" ]]; then
+            echo "[Info] folder_backup option is set, uploading ${FOLDER_BACKUP}"
+            ./dropbox_uploader.sh -s -f /etc/uploader.conf upload /${backup_dir} "${OUTPUT_DIR}/folder_backup/${backup_dir}"
         fi
     else
         # received undefined command
