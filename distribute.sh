52708f25691b5414fb4c8912b660bcb9e8df8b0b
jaseonline <74970326+jaseonline@users.noreply.github.com>
2022-04-09 01:45:19 +0100

    Update distribute.sh

--- distribute.sh
+++ distribute.sh
@@ -24,6 +24,6 @@ docker run -it --rm --privileged --name "${ADDON_NAME}" \
     --all \
     --push \
     --from "homeassistant/{arch}-base" \
-    --author "Daniel Welch <dwelch2102@gmail.com>" \
+    --author "Jason Titter <jason.titter@gmail.com>" \
     --doc-url "${GITHUB_URL}"
 
